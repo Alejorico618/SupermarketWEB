b32b460d99007524a7257a9c9c5c295e7c6a2e7a
Alejorico618 <d.alejandrorico2@gmail.com>
2025-05-09 15:53:58 -0500

    Create Providers y agregar pages

--- Models/PayMode.cs
+++ Models/PayMode.cs
@@ -1,6 +1,15 @@
-﻿namespace SupermarketWEB.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SupermarketWEB.Models
 {
     public class PayMode
     {
+        public int Id { get; set; }
+
+        [Required]
+        public string? Description { get; set; }
+
+        [Required]
+        public bool IsActive { get; set; }
     }
 }
