c60032f659fe762a2956e81bddc07cdf6d579ea4
Alejorico618 <d.alejandrorico2@gmail.com>
2025-05-09 17:23:57 -0500

    Edit Providers

--- Pages/Providers/Edit.cshtml.cs
+++ Pages/Providers/Edit.cshtml.cs
@@ -1,12 +1,41 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Models;
 
 namespace SupermarketWEB.Pages.Providers
 {
     public class EditModel : PageModel
     {
-        public void OnGet()
+        private readonly SupermarketWEB.Data.SupermarketContext _context;
+
+        public EditModel(SupermarketWEB.Data.SupermarketContext context)
         {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Provider Provider { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var provider = await _context.Providers.FindAsync(id);
+            if (provider == null) return NotFound();
+
+            Provider = provider;
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid) return Page();
+
+            _context.Attach(Provider).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
         }
     }
 }
