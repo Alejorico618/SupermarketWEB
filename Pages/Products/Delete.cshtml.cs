20d2764ce31dcf7822b57322ffa06a2709ca1895
Alejorico618 <d.alejandrorico2@gmail.com>
2025-05-10 14:41:22 -0500

    Delete Products

--- Pages/Products/Delete.cshtml.cs
+++ Pages/Products/Delete.cshtml.cs
@@ -1,12 +1,45 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SupermarketWEB.Models;
 
 namespace SupermarketWEB.Pages.Products
 {
     public class DeleteModel : PageModel
     {
-        public void OnGet()
+        private readonly SupermarketWEB.Data.SupermarketContext _context;
+
+        public DeleteModel(SupermarketWEB.Data.SupermarketContext context)
         {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Product Product { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null) return NotFound();
+
+            Product = await _context.Products.FindAsync(id);
+
+            if (Product == null) return NotFound();
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var product = await _context.Products.FindAsync(id);
+
+            if (product != null)
+            {
+                _context.Products.Remove(product);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
         }
     }
 }
