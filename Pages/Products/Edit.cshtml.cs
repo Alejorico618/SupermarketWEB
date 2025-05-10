a85afb1313156d8709105682e514e86673e5fe39
Alejorico618 <d.alejandrorico2@gmail.com>
2025-05-10 14:32:14 -0500

    Edit Products

--- Pages/Products/Edit.cshtml.cs
+++ Pages/Products/Edit.cshtml.cs
@@ -1,12 +1,41 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Models;
 
 namespace SupermarketWEB.Pages.Products
 {
     public class EditModel : PageModel
     {
-        public void OnGet()
+        private readonly SupermarketWEB.Data.SupermarketContext _context;
+
+        public EditModel(SupermarketWEB.Data.SupermarketContext context)
         {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Product Product { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var product = await _context.Products.FindAsync(id);
+            if (product == null) return NotFound();
+
+            Product = product;
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid) return Page();
+
+            _context.Attach(Product).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
         }
     }
 }
