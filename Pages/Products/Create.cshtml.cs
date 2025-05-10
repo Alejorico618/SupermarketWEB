dd06093e1b174f4c5447a19c2672095196299669
Alejorico618 <d.alejandrorico2@gmail.com>
2025-05-10 14:01:12 -0500

    Create  Products

--- Pages/Products/Create.cshtml.cs
+++ Pages/Products/Create.cshtml.cs
@@ -1,12 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SupermarketWEB.Models;
 
 namespace SupermarketWEB.Pages.Products
 {
     public class CreateModel : PageModel
     {
-        public void OnGet()
+        private readonly SupermarketWEB.Data.SupermarketContext _context;
+
+        public CreateModel(SupermarketWEB.Data.SupermarketContext context)
         {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Product Product { get; set; }
+
+        public IActionResult OnGet() => Page();
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid || Product == null)
+                return Page();
+
+            _context.Products.Add(Product);
+            await _context.SaveChangesAsync();
+            return RedirectToPage("./Index");
         }
     }
 }
